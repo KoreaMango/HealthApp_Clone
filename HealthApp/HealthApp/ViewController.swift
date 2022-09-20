4390d3b704ffef7755774ada85adc35f44c70512
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-09-20 18:43:29 +0900

    Project Init

---
+++ HealthApp/HealthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthApp
+//
+//  Created by 강민규 on 2022/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
