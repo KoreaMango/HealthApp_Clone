1fb931a19ab6956151ccf7404c907315e54fedb8
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-09-27 15:43:24 +0900

    Detail Point Add, Navigation Add

--- HealthApp/HealthApp/ViewController.swift
+++ HealthApp/HealthApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sleepView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(goPage(sender:)))
+        self.sleepView.addGestureRecognizer(gesture)
+        
     }
-
-
+    
+    @objc func goPage(sender : UIGestureRecognizer){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let nextVC = storyboard.instantiateViewController(withIdentifier: "nextVC")
+        
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
 }
 
